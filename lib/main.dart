6400e36d400a976869b9b46fa82eb3e1b22dba3d
Reo <charimelmariano27@gmail.com>
2025-04-13 22:41:13 +0800

    updated model with 90 accuracy

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:nutrishow/views/splash.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const MyApp());
 }
 
